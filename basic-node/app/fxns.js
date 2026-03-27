fd9cca3fdb878452416e17ee16ca8113c3c5aee5
njsoly <njsoly@syntj.com>
2026-03-27 02:12:40 -0500

    basic-node/: initialized node project, added fxns.js

---
+++ basic-node/app/fxns.js
@@ -0,0 +1,11 @@
+// noinspection JSUnusedGlobalSymbols
+
+module.exports = {
+    luckyNumber: () => {
+        return 29;
+    },
+
+    nanosToMillis: (nanos) => {
+        return nanos / 1000000;
+    }
+}
--- basic-node/app/index.js
+++ basic-node/app/index.js
@@ -1,1 +1,8 @@
+// noinspection JSUnusedLocalSymbols
+const { readFile, readFileSync } = require('fs')
+
+readFile('./stuff-to-read.txt', 'utf8', (err, data) => {
+    console.log(data)
+})
+
 console.log('Hello, world!')
