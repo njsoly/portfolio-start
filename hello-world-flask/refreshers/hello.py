1322e2889e15f9a4132feb31944d7660c0d6d2ae
njsoly <njsoly@syntj.com>
2026-06-02 18:16:11 -0500

    hello flask refreshers: tidying and tweaking

--- hello-world-flask/refreshers/hello.py
+++ hello-world-flask/refreshers/hello.py
@@ -14,19 +14,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-    p = '110000'
-    regex_alternating_repetitive_digit_pair = r"(?=([0-9])[0-9]\1)"
-    print(re.findall(regex_alternating_repetitive_digit_pair, p))
-    print()
-
-    n = int('5')
-    arr = list(map(int, '2 3 6 6 5'.split()))
-    arr.sort(reverse=True)
-    for i in arr[1::]:
-        if i != arr[0]:
-            print(i)
-            break
-    print(str(arr))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
